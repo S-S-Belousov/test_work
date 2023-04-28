1de4de8064141e3d2163621744adb497e1b8dc3d
Sergey <108421600+S-S-Belousov@users.noreply.github.com>
2023-04-28 10:11:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from random import randint
+
+
+class SearchForDuplicateNumbers:
+    def __init__(self, nums: list[int]):
+        self.nums = nums
+
+    def findDuplicate(self) -> list[int]:
+        result_set = set()
+        for i in range(len(self.nums)-1):
+            if self.nums[i] in self.nums[i+1:]:
+                result_set.add(self.nums[i])
+        return list(result_set)
+
+
+if __name__ == '__main__':
+    n = int(input('Введите N: '))
+    nums = [randint(1, n) for i in range(n+2)]
+    print(f'Сгенерированные числа: {nums}')
+    solution = SearchForDuplicateNumbers(nums)
+    result = solution.findDuplicate()
+    print(
+        'Повторяющиеся числа: {}'.format(result)
+        )
