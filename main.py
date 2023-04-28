1493926184757a045678caf7ff6fe8faad2b989e
Sergey <108421600+S-S-Belousov@users.noreply.github.com>
2023-04-28 12:33:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,17 @@ class SearchForDuplicateNumbers:
     def __init__(self, nums: list[int]):
         self.nums = nums
 
-    def findDuplicate(self) -> list[int]:
+    def find_duplicate(self) -> list[int]:
         result_set = set()
-        for i in range(len(self.nums)-1):
-            if self.nums[i] in self.nums[i+1:]:
+        for i in range(len(self.nums) - 1):
+            if self.nums[i] in self.nums[i + 1:]:
                 result_set.add(self.nums[i])
         return list(result_set)
 
+
 if __name__ == '__main__':
     n = int(input('Введите N: '))
-    nums = [randint(1, n) for i in range(n+2)]
-    print(f'Сгенерированные числа: {nums}')
+    nums = [randint(1, n) for i in range(n + 2)]
+    print(f'Сгенерированные числа:', nums)
     solution = SearchForDuplicateNumbers(nums)
-    print(
-        'Повторяющиеся числа: {}'.format(
-        solution.findDuplicate()
-        )
-        )
-    
+    print('Повторяющиеся числа:', solution.find_duplicate())
